2ed5874bc2137a4cbc7924040ef1aee3ee7411f3
JT <jtvinolus@gmail.com>
2022-05-15 01:35:01 -0700

    adding arguments and logic for get and post

--- BE/query_api.py
+++ BE/query_api.py
@@ -1,15 +1,26 @@
 from flask import request
 from flask_restful import Api, Resource, reqparse
+from database import database
 
 
+query_get_args = reqparse.RequestParser()
+query_get_args.add_argument("")
+
+
+query_post_args = reqparse.RequestParser()
+query_post_args.add_argument("tags", required=True)
+
+db = database()
 
 class query_API(Resource):
 
-    def post():
-        pass
-
+    #Get a list of available broadcasters and game categories
     def get():
-        pass
+        return 200, db.all_tags()
+    
+    #Make a clip query
+    def post():
+        return 200 #Recommendation System Call
 
     def delete():
         pass
