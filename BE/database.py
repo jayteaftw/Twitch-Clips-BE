79632d97363791aa4d18ff49cefd8b3d1b90825b
JT <jtvinolus@gmail.com>
2022-05-15 01:34:38 -0700

    First Init

---
+++ BE/database.py
@@ -0,0 +1,16 @@
+
+
+class database():
+
+    def __init__():
+        pass
+
+    def insert(table_name, data):
+        pass
+
+    def query(table_name, data):
+        pass
+
+    def all_tags():
+        pass
+    
