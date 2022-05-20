60e3f2fd583d2514f65b2be9ad9ee417b207feca
Brandon Quant <brandonquant@Brandons-MacBook-Pro-2.local>
2022-05-19 18:06:36 -0700

    Updated to work with twitch api calls

--- BE/recommender.py
+++ BE/recommender.py
@@ -1,31 +1,62 @@
-from flask import Flask, json, session, jsonify
-from flaskext.mysql import MySQL
+#from flask import Flask, json, session, jsonify
+#from flaskext.mysql import MySQL
 from datetime import datetime
 from datetime import timedelta
+#from .db_models import User, Tag, Twitch_URL
+#from run import db
+#import db_models 
+
 import twitch_api
 
-#twitch API
-GET_TAGS_FOR_SPECIFIC_CHANNEL = "curl -X GET 'https://api.twitch.tv/helix/streams/tags?bradcaster_id={} -H 'Authorization: Bearer {}' -H 'Client-Id: {}"
-
-#based on clip ID, broadcaster_id, or game ID
-GET_CLIPS = "curl -X GET 'https://api.twitch.tv/helix/clips?id={}' -H 'Authorization: Bearer {}' -H 'Client-Id: {}'"
-
-#get first page of stream tags that Twitch defines
-GET_ALL_TAGS = "curl -X GET 'https://api.twitch.tv/helix/tags/streams' -H 'Authorization: Bearer {}' -H 'Client-Id: {}'"
-
-
-#name of database fields will change based on implementation  
-'''
-Login Table: Username, password
-Users Liked Categories Table: UserId, GameName, GameId
-Data Table: GameId, Url, Timestamp, NumViews
-'''
-
-#@app.route('/getClips/<tag1>/<tag2>/<tag3>/<tag4>/<tag5>')
-def get_clips(user_id, game_name):
-	twitch_api = twitch_api()
+def get_clips(user_id, tags):
+	twitch_inst = twitch_api.twitch_api()
 	now = datetime.now()
-	time_start = twich_api.convert_time_RC3339(now.year, now.month, now.day, now.hour, now.minute, now.second) 	
+	#print("NOW: " + str(now))
+	time_start = twitch_inst.convert_time_RC3339(now.year, now.month, now.day, now.hour, now.minute, now.second) 	
+	#print("TIME START: " + str(time_start))
+	time_end_hour = int(now.hour) + 2
+	time_end = twitch_inst.convert_time_RC3339(now.year, now.month, now.day, time_end_hour, now.minute, now.second)
+	#print("TIME END: " + str(time_end))
+
+	'''grabbing game_name from DB'''
+	#game_names = Tag.query.filter_by(id=user_id).first().with_entities(Tag.tags)
+	#db_query = Twitch_URL.query.filter_by(tag_id = game_id) #NEED TO FINISH
+
+	game_names_from_DB = "VALORANT,League of Legends"
+	game_names_list = game_names_from_DB.split(',')
+
+	urls = []
+	thumbnail_urls = []
+
+	
+	for game in game_names_list:
+		print("GAME: " + str(game))
+		game_id = twitch_inst.get_game_ID(str(game)) #replaced with game_names
+		print("GAME_ID: " + str(game_id))
+		message = []
+		message = twitch_inst.get_game_clips(game_id, time_start, time_end)
+		#print(message.json()['data'])
+
+		for entry in message.json()['data']:
+			entry_id = entry['id']
+			entry_ts = entry['created_at']
+			entry_url = entry['url']
+			entry_thumbnail_url = entry['thumbnail_url']
+			entry_game_id = entry['game_id']
+			#print("ID :" + str(entry_id) + ", TS: " + str(entry_ts) + ", URL: " + str(entry_url) + " THUMBNAIL: " + str(entry_thumbnail_url))
+			urls.append(entry_url)
+			thumbnail_urls.append(entry_thumbnail_url)
+			
+			'''adding urls from TWITCH API to database'''
+			#twitch_url_entry = Twitch_URL(id = '', url = str(entry_url), date = entry_ts, tag_id = entry_game_id)
+			#db.session.add(twitch_url_entry)
+		#db.session.commit()
+		
+	#print(urls)
+	#print(thumbnail_urls)
+	return urls, thumbnail_urls
+
+	'''
 	try:
 		if session.get('user'):
 			_id = request.form['id']
@@ -69,7 +100,8 @@ def get_clips(user_id, game_name):
 		print("Bad Request to DB")
 		return
 		#return render_template('error.html', error = 'Bad Request')
+	'''
 
-
-
+if __name__ == "__main__":
+	get_clips("1234", "test")
 
