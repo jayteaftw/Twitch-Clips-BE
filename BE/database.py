3ca66bec055a1c20b0774ecb616d0297675fbe75
Brandon Quant <brandonquant@Brandons-MacBook-Pro-2.local>
2022-05-23 00:13:58 -0700

    Implemented some functions

--- BE/database.py
+++ BE/database.py
@@ -43,31 +43,86 @@ class database():
 
     def checkIfValidUser(self, email, token):
         """ Check if user is valid with specific token. Return true or false """
+        sql = "SELECT * FROM User WHERE email = '%s' LIMIT 1"
+        get_login_info = sql % (email)
+        print("GET LOGIN INFO: " + get_login_info)
+        data = self.query(get_login_info) #should return the login info from DB
+        if data:
+            return True
+        else:
+            return False
+
         return True
 
     def checkIfValidUserPass(self,email, password):
         """ If User is valid return a generated token
         If not return False """
+        sql = "SELECT * FROM User WHERE email = '%s' and password = '%s' LIMIT 1"
+        get_login_info = sql % (email, password)
+        print("GET LOGIN INFO: " + get_login_info)
+        data = self.query(get_login_info) #should return the login info from DB
+        if not data:
+            return False
+        else:
+            for row in data:
+                return row[4]
         
-        return True
+        
+        #return True
 
-    def createNewUser(self, email, password, name):
+    def createNewUser(self, email, password): #, name):
         """ If User is created, return generated token
         If User is not created, return False """
+        sql = "SELECT * FROM User WHERE email = '%s' and password = '%s' LIMIT 1"
+        get_login_info = sql % (email, password)
+        print("GET LOGIN INFO: " + get_login_info)
+        data = self.query(get_login_info) #should return the login info from DB
+        if not data:
+            return False
+        else:
+            for row in data:
+                return row[4]
 
-        return True
+        #return True
 
     def getUserCategories(self, email):
         "Return a list of user selected categories"
+        sql = "SELECT * FROM User WHERE email = '%s' LIMIT 1"
+        get_login_info = sql % (email)
+        print("GET LOGIN INFO: " + get_login_info)
+        data = self.query(get_login_info)
+        data = [[1, 'test@gmail.com', 'test_password', 'VALORANT,APEX LEGENDS']]
+        for row in data:
+            return row[3]
 
-        return ["Valorant", "CSGO", "Apex Legends", "League of Legends"]
+        #return ["Valorant", "CSGO", "Apex Legends", "League of Legends"]
 
     def getAllCategories(self):
         "Return a list of all categories"
-
+        
         return ["Valorant", "CSGO", "League of Legends", "Minecraft", "Apex Legends", "Call of Duty WarZone", "Among Us", "Dota 2", "World of Warcraft" ]
 
 
     def setUserCategories(self, categories, email):
         "Takes categories and insert them into specific user cell"
         return True
+
+if __name__ == "__main__":
+    database = database()
+    print("__________________________________")
+    print("checkIfValidUser TEST")
+    IsValidUser = database.checkIfValidUser("test@gmail.com", "test_token")
+    print(IsValidUser)
+    print("__________________________________")
+    print("checkIfValidUserPass TEST")
+    IsValidUserPass = database.checkIfValidUserPass("test@gmail.com", "test_password")
+    print(IsValidUserPass)
+    print("__________________________________")
+    print("createNewUser TEST")
+    createUser = database.createNewUser("test@gmail.com", "test_password")
+    print(createUser)
+    print("__________________________________")
+    print("getUserCategories TEST")
+    user_categories = database.getUserCategories("test@gmail.com")
+    print(user_categories)
+    print("__________________________________")
